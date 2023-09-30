3132bcbf0753b545af9d0189f5e956cdbd899eda
shahana49 <145976313+shahana49@users.noreply.github.com>
2023-09-30 03:46:23 -0700

    Add files via upload

---
+++ positive list.py
@@ -0,0 +1,6 @@
+#program to generate positive list of numbers from a given list of integers
+list=[-2,-45,67,68,-39,50,60,26,]
+print("the positive number in list are:")
+for i in list:
+    if i>0:
+        print(i)
